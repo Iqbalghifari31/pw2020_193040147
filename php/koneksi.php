362ff368d4b416627ac1638ffa88a69e1b1d31bc
Mohammad Iqbal Ghifari <60709403+Iqbalghifari31@users.noreply.github.com>
2020-05-18 07:58:23 +0700

    Delete koneksi.php

--- php/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $conn = new mysqli("localhost", "root", "", "ifc");
-  date_default_timezone_set("Asia/Jakarta");
-  echo date("l")." ";
-  echo date("d-m-Y") . " ";
-  echo date("h:i:s");
-?>
